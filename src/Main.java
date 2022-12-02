adc1e87a571f8e36d960b526740bddb61b0111d5
Максим <gmax7911@gmail.com>
2022-12-02 16:21:07 +0300

    1 задача

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задача 1
+
+        int age = 15;
+        if(age >= 18){
+            System.out.println("Поздравляем! Вам 18 или больше!");
+            if(age < 18){
+                System.out.println(" Вам еще нет 18, нужно немного подождать.");
+            }
+        }
+    }
+}
