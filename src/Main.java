0d976f4d270b79bf01fcb3ecca3e67f932a8f1ad
Максим <gmax7911@gmail.com>
2022-12-02 16:32:05 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-
         // Задача 1
 
         int age = 15;
-        if(age >= 18){
+        if(age >= 18)
+        {
             System.out.println("Поздравляем! Вам 18 или больше!");
-            if(age < 18){
+        }
+        if(age < 18)
+            {
                 System.out.println(" Вам еще нет 18, нужно немного подождать.");
             }
+
+
+
+        // Задача 2
+
+        int babyAge = 25;
+        if (babyAge >= 7)
+        {
+            System.out.println("Ребенок ходит в школу");
         }
+        if(babyAge >= 18)
+
+    {
+        System.out.println("человек уже закончил школу и может отправляться в университет");
     }
+        if (babyAge >= 24)
+        {
+            System.out.println("человек окончил университет и ему пора искать первую работу");
+        }
+
+
+
+
+
+    }
+
 }
